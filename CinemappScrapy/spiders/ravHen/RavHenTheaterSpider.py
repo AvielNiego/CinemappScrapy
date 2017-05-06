e7e9cd11efab9e4211ee4372e6c54c2aa43148e9
Aviel <avielniego@gmail.com>
2017-05-06 15:27:28 +0300

    change rav hen logo url

--- CinemappScrapy/spiders/ravHen/RavHenTheaterSpider.py
+++ CinemappScrapy/spiders/ravHen/RavHenTheaterSpider.py
@@ -5,7 +5,7 @@ from CinemappScrapy.spiders.globalSpiders.TheaterSpider import TheaterSpider
 
 class RavHenTheaterSpider(TheaterSpider):
     def get_logo_url(self):
-        return "http://www.rav-hen.co.il/media/iti_rh/imgs/favicon.gif"
+        return "http://ticketnet.co.il/imgs/GIF/Affiliates/Rav-Hen.co.il.gif"
 
     name = "RavHen_Theater_Spider"
 
